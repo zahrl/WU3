a97a11aa7221e561bd2ad5b2ab408d57050b6579
Dr. Paul R. Zahrl <prz@paulrzahrl.com>
2017-11-14 21:24:23 +0100

    chg: Add assertions

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,12 +1,29 @@
 from random import randint
 
-def output_winner (computer_score, user_score):
+class Error(Exception):
+    pass
+
+class ValueTooSmallError(Error):
+    pass
+
+class ValueTooLargeError(Error):
+    pass
+
+def output_round_winner (computer_score, user_score):
     if computer_score < user_score:
         print('Congratulations, you won this round!')
     elif computer_score > user_score:
         print('Sorry, this time you lost this round!')
     elif computer_score == user_score:
         print('Nobody won this round!')
+
+def compute_result (computer, user):
+    if computer == user:
+        return -1
+    elif ((computer == 1 and user == 2) or (computer == 2 and user == 3) or (computer == 3 and user == 1)):
+        return 1
+    elif ((computer == 1 and user == 3) or (computer == 2 and user == 1) or (computer == 3 and user == 2)):
+        return 0
 
 current_round = 1
 tries = 3
@@ -16,19 +33,32 @@ user_won = 0
 while current_round <= tries:
     try:
         user = int(input("What are you? (1 = Scissors, 2 = Rock, 3 = Paper)"))
-    except ValueError:
+        if user < 1:
+            raise ValueTooSmallError
+        elif user > 3:
+            raise ValueTooLargeError
+    except TypeError:
         print("Please enter a valid value!")
+    except ValueTooSmallError:
+        print("The value is too small!")
+    except ValueTooLargeError:
+        print("The value is too large!")
     else:
         computer = randint(1, 3)
+
         if (computer == user):
+            assert(compute_result(computer, user)) ==  -1
             print("Tie!")
         elif ((computer == 1 and user == 2) or (computer == 2 and user == 3) or (computer == 3 and user == 1)):
-            computer_won += 1
-            print('Sorry, you lost!')
-        elif ((computer == 1 and user == 3) or (computer == 2 and user == 1) or (computer == 3 and user == 2)):
+            assert(compute_result(computer, user)) ==  1
             user_won += 1
             print('You won!')
+        elif ((computer == 1 and user == 3) or (computer == 2 and user == 1) or (computer == 3 and user == 2)):
+            assert(compute_result(computer, user)) ==  0
+            computer_won += 1
+            print('Sorry, you lost!')
+            
         print('Computer was ' + str(computer))
         current_round += 1;
 
-output_winner(computer_won, user_won)
+output_round_winner(computer_won, user_won)
